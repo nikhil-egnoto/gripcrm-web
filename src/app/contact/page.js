bf07ce4082f621ed39f446981bfbe24ff93700a3
nikhil-egnoto <nikhil.a@egnoto.com>
2026-09-11 15:36:18 +0530

    update

---
+++ src/app/contact/page.js
@@ -0,0 +1,11 @@
+export default function Contact() {
+    return (
+        <main>
+            <section className="bg-blue-50 py-16 md:py-24 relative overflow-hidden">
+                <div className="container mx-auto">
+                    Contact us
+                </div>
+            </section>
+        </main>
+    )
+}
